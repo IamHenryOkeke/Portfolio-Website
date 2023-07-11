07e84a7c18b15ea8980d139c4c0acc8b7b810da1
Henry Okeke <iamhenryokeke@gmail.com>
2023-07-12 00:21:02 +0100

    ui fixes

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="text-center text-sm md:text-base">
+      © 2023 NmesomaHenry All Rights Reserved.
+    </div>
+  )
+}
+
+export default Footer
