d85711f513ccc11590a3a4c1527f55e0bdb96fe3
Henry Okeke <iamhenryokeke@gmail.com>
2023-07-11 21:02:46 +0100

    implemented tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        body : ['Montserrat', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
