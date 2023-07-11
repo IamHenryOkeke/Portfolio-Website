e921b65a8dd19071c2134ccd120abd721e7266cf
Henry Okeke <iamhenryokeke@gmail.com>
2023-07-11 21:04:39 +0100

    project init

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import NavBar from "./components/NavBar/NavBar"
 
 const App = () => {
   return (
-    <>
+    <div className="bg-[#191a1f] font-body text-white">
       <NavBar/>
-    </>
+    </div>
   )
 }
 
