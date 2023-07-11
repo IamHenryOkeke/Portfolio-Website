9fdffb29ab7d02de24ffb8a479ae95ef13dcf329
Henry Okeke <iamhenryokeke@gmail.com>
2023-07-12 00:22:03 +0100

    Component imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import ContactCard from "./components/ContactMe/ContactMe"
+import Footer from "./components/Footer/Footer"
 import NavBar from "./components/NavBar/NavBar"
+import Skillsets from "./components/Skillsets/Skillsets"
+
 
 const App = () => {
   return (
-    <div className="bg-[#191a1f] font-body text-white">
+    <div className="bg-[#130b0f] font-body text-[#f0e5ea]">
       <NavBar/>
+      <Skillsets/>
+      <ContactCard twitterUrl="https://twitter.com/NmesomaHenry" portfolioUrl='https://github.com/IamHenryOkeke' phone='tel:+2347080194374' linkedInUrl='http://www.linkedin.com/in/henry-okeke-3176481a9' emailAddress="iamhenryokeke@gmail.com"/> 
+      <Footer/>
     </div>
   )
 }
